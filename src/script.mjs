163fb4832817cd5161f933ac3b0f6bb9dc0ed74c
Tore Mæland Hirth <torehirth@gmail.com>
2024-12-12 20:24:09 +0100

    destructured object and array

--- src/script.mjs
+++ src/script.mjs
@@ -1,8 +1,23 @@
-const title = () => {
-  const documentHeading = document.querySelector("h1");
-  documentHeading.innerHTML = "Updated heading";
+// Destructure the properties from the 'car'
+// object below and then log all of them
 
-  console.log(documentHeading);
+const car = {
+  manufacturer: "Nissan",
+  model: "Skyline GT-R",
+  country: "Japan",
+  coupe: true,
 };
 
-title();
+const { manufacturer, model, country, coupe } = car;
+console.log(manufacturer);
+
+// Destructure the first, second and fifth value from
+// the array. You are free to call the variables whatever
+// you would like. Once you have destructured the values,
+// log the variables so you know that you have destructured
+// them correctly.
+
+const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const [firstValue, secondValue, , , fifthValue] = values;
+
+console.log(firstValue, secondValue, fifthValue);
